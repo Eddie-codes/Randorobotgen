2e68d37f70a9676b621aa47b5b97645aa1e77c50
Eddie <egoodwin848@hotmail.co.uk>
2021-03-24 21:04:26 +0000

    first iteration of project

---
+++ src/Card.js
@@ -0,0 +1,24 @@
+//First component to be created in this app. It is a card storing user details and their image
+//images are coming in from API of https://robohash.org
+
+//importing react
+import React from 'react';
+
+// creating function to pass in properties in form of props and returns the profile image from the robo
+//robohash website
+const Card = ({name, email, id}) => {
+    return(
+        //using tachyons to add styling to each card
+        //taking argument of props from the ID field of robots.js file. Need to use "`" character
+        //to create template strings which allows you to use JS symbol ${}
+        <div className='tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5 '>
+            <img alt='profile image' src={`https://robohash.org/${id}?200x200`}/>
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'the goose',
+        username: 'Moose',
+        email: 'randoemail@gmaieel.com'
+    },
+    {
+        id: 2,
+        name: 'Dr Shoe',
+        username: 'Gustavo',
+        email: 'Shoeman@gmaieel.com'
+    },
+    {
+        id: 3,
+        name: 'the fruice',
+        username: 'Jugo de naranja',
+        email: 'español@gmaieel.com'
+    },
+    {
+        id: 4,
+        name: 'Tractor Beam',
+        username: 'Aliem',
+        email: 'alien@gmaieel.com'
+    },
+    {
+        id: 5,
+        name: 'the five',
+        username: 'Cinco',
+        email: 'cincodeclowno@gmaieel.com'
+    },
+]
